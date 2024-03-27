1fef79e1d506a3491c8a1b66678db0e27822dc17
ozprey <152182347+ozprey@users.noreply.github.com>
2024-03-27 20:55:17 +0530

    Update mm.js

--- mm.js
+++ mm.js
@@ -1,6 +1,8 @@
 const apikey = 'api_key=9782bcec11edfd02dbd2f55b513f79b8';
 const burl = 'https://api.themoviedb.org/3';
 const imgurl = 'https://image.tmdb.org/t/p/w500'
+const appnd = '&append_to_response=credits';
+
 
 
 let it = document.getElementById('form');
@@ -20,11 +22,10 @@ async function malabar(x) {
         const tj = await fd.json();  //2 JSON TO JS OBJECT
         const res = tj.results;    //3 REDUCED OBJECT WITH ONLY RESULTS
         const idcoll = Object.values(res).map(innerObj => innerObj.id);  //4 EXTRACTING ID PROPERTY OF REDUCED OBJECT
-        console.log(idcoll);
 
         idcoll.forEach(e =>    {                   //5 LOOPING THROUGH EACH ID
-            sabar(burl + `/movie/${e}?&` + apikey)
-            console.log(e)}
+            sabar(burl + `/movie/${e}?&` + apikey + appnd)
+           }
             );
     } catch (error) {
         console.error('amigo',error)
@@ -37,19 +38,21 @@ async function sabar(x) {
     try {
         const fd = await fetch(x);   //6.1 FETCHED DATA
         const tj = await fd.json();  //6.2 JSON TO JS OBJECT
+        const direc = tj.credits.crew.find(member => member.job === 'Director');
+
         const { original_title,title, id, backdrop_path, runtime ,overview ,tagline} = tj;  //6.3 reduced js object WITH ONLY CERTAIN PROPS
         const mj = { original_title,title, id, backdrop_path,runtime,overview ,tagline};
-          console.log(mj.overview);
         const dfd = document.createElement('div');  // CREATING DOM ELEMENT
+        const dj =console.log(direc.name);
         dfd.innerHTML= 
     `<div class="one">
     <div class="three"><span class="plot">${mj.overview}</span></div>
     <div class="to">
-        <img src="${imgurl+mj.backdrop_path}" alt="${id}" onerror="this.onerror=null;this.style.display='none';>
+        <img src="${imgurl+mj.backdrop_path}" alt="${id}" onerror="this.onerror=null;this.src='mmm.jpg;'>
     </div>
     <div class="textblock">
         <p class="title">${mj.title}</p>
-        <p class="crd"><span class="year">${mj.original_title}</span><span class="ids"><i>${mj.id}</i></span><span
+      <p class="crd"><span class="year">${direc.name}</span><span
                 class="duration">${mj.runtime} min</span>
         </p>
         <p class="tagline"><i>${mj.tagline}</i></p>
